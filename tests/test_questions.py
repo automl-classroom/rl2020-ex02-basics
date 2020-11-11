f2801c1e86347e6e52d12ed4bbabaa96f6cbd50e
Theresa Eimer <eimer@tnt.uni-hannover.de>
2020-11-11 16:28:20 +0100

    question test

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -8,8 +8,8 @@ class TestQuestions(unittest.TestCase):
 
         self.assertTrue(lines[0].startswith('1') or lines[0].startswith('One') or lines[0].startswith('one'))
         self.assertTrue(lines[1].startswith('reward') or lines[1].startswith('Reward') or lines[1].startswith('R ') or lines[1].startswith('The reward'))
-        self.assertTrue(lines[2].startswith('member 3: '))
-        self.assertTrue(lines[3].startswith('member 3: '))
+        self.assertTrue(lines[2].startswith('v'))
+        self.assertTrue(lines[3].startswith('p'))
         self.assertTrue(lines[4].startswith('Yes') or lines[4].startswith('yes'))
 
 
