b01a04cbb6553e603ac6563593f426e807a06351
Fabian <fab.beringer@gmail.com>
2020-11-16 17:13:30 +0100

    reformat

--- solutions.py
+++ solutions.py
@@ -1,13 +1,15 @@
 import numpy as np
 from env import MarsRover
 
-def update_value_function(v, state, new_state, action, reward, gamma=0.9):
+
+def update_value_function(v, state, new_state, reward, gamma=0.9):
     done = False
     new_v = np.copy(v)
     new_v[state] = reward + gamma * v[new_state]
     if np.linalg.norm((v - new_v), ord=1) <= 0:
         done = True
     return new_v, done
+
 
 def update_policy(qs, pi, state, new_state, action, reward, gamma=0.9):
     done = False
@@ -17,7 +19,8 @@ def update_policy(qs, pi, state, new_state, action, reward, gamma=0.9):
         done = True
     return qs, new_pi, done
 
-def run_policy_iteration(transition_probabilities=np.ones((5,2)), rewards=[1, 0, 0, 0, 10], horizon=10):
+
+def run_policy_iteration(transition_probabilities=np.ones((5, 2)), rewards=[1, 0, 0, 0, 10], horizon=10):
     env = MarsRover(transition_probabilities, rewards, horizon)
     qs = np.zeros((5, 2))
     pi = np.random.randint(0, 2, size=5)
@@ -41,7 +44,9 @@ def run_policy_iteration(transition_probabilities=np.ones((5,2)), rewards=[1, 0,
 
     return pi, i, final_reward
 
-def run_value_iteration(transition_probabilities=np.ones((5,2))*0.5, rewards=[1, 0, 0, 0, 10], horizon=10, gamma=0.9):
+
+def run_value_iteration(transition_probabilities=np.ones((5, 2)) * 0.5, rewards=[1, 0, 0, 0, 10], horizon=10,
+                        gamma=0.9):
     v = np.zeros(5)
     env = MarsRover(transition_probabilities, rewards, horizon)
     done = False
@@ -50,13 +55,13 @@ def run_value_iteration(transition_probabilities=np.ones((5,2))*0.5, rewards=[1,
     while not done:
         i += 1
         print(f"This is step {i}")
-        r1=(rewards[state-1]+gamma*v[state-1])
-        r2=(rewards[state+1]+gamma*v[state+1])
+        r1 = (rewards[state - 1] + gamma * v[state - 1])
+        r2 = (rewards[state + 1] + gamma * v[state + 1])
         action = np.argmax([r1, r2])
         if r1 == r2:
             action = np.random.randint(2)
         new_state, reward, done = env.step(action)
-        v, converged = update_value_function(v, state, new_state, action, reward)
+        v, converged = update_value_function(v, state, new_state, reward)
         if converged:
             break
         state = new_state
@@ -66,6 +71,7 @@ def run_value_iteration(transition_probabilities=np.ones((5,2))*0.5, rewards=[1,
     print(f"Your agent achieved a final accumulated reward of {final_reward} after {i} update steps.")
 
     return v, i, final_reward
+
 
 def evaluate_policy(pi, env):
     state = env.reset()
@@ -77,12 +83,13 @@ def evaluate_policy(pi, env):
         r_acc += reward
     return r_acc
 
+
 def evaluate_agent(v, env):
     state = env.reset()
     done = False
     r_acc = 0
     while not done:
-        action = max(v[max(state-1, 0)], v[min(state+1, 5)])
+        action = max(v[max(state - 1, 0)], v[min(state + 1, 5)])
         new_state, reward, done = env.step(action)
         r_acc += reward
     return r_acc
